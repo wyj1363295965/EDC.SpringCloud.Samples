84972b999eb848f6d6a7642abf8f3ec3d6f61e97
wyj <15927717572@163.com>
2023-03-21 10:34:11 +0800

    pr

--- src/part2_client-load-balance/user-service/src/main/java/com/mbps/cd/userservice/web/UserController.java
+++ src/part2_client-load-balance/user-service/src/main/java/com/mbps/cd/userservice/web/UserController.java
@@ -2,6 +2,7 @@ package com.mbps.cd.userservice.web;
 
 import com.mbps.cd.userservice.domain.User;
 import com.mbps.cd.userservice.domain.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @author EdisonZhou
  */
 @RestController
+@Slf4j
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public User findById(@PathVariable Long id){
+    public User findById(@PathVariable Long id) {
+
         User result = userRepository.findOne(id);
+        log.info("result:{}", result);
         return result;
     }
 }
